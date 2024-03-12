83d396f8d4a16ea0297c15ca8915dc4974064802
ilya07092000 <ischenko2012@gmail.com>
2024-03-12 14:12:50 +0200

    feat: BasicDTO

--- src/infrastructure/contracts/BasicDTO.ts
+++ src/infrastructure/contracts/BasicDTO.ts
@@ -1,13 +1,14 @@
 import { SnakelizeKeys } from '../types/common/camelToSnake';
+import { CamelizeKeys } from '../types/common/camelizeKeys';
 
 /**
  * abstract class for dto
  */
-abstract class BasicDto<T extends object> {
+abstract class BasicDto {
   /**
    * make all keys from snake_case to camelCase
    */
-  present() {
+  present(): CamelizeKeys<this> {
     return Object.keys(this).reduce((acc, propName) => {
       const camelKey = propName.replace(/_([a-z])/g, (_, c) => c.toUpperCase());
       if (['createdAt', 'updatedAt'].includes(camelKey)) {
@@ -16,13 +17,13 @@ abstract class BasicDto<T extends object> {
 
       acc[camelKey] = this[propName];
       return acc;
-    }, {});
+    }, {}) as CamelizeKeys<this>;
   }
 
   /**
    * make all keys from camelCase to snake_case
    */
-  data(): SnakelizeKeys<T> {
+  data(): SnakelizeKeys<this> {
     return Object.keys(this).reduce((acc, propName) => {
       const camelKey = propName.replace(
         /([A-Z])/g,
@@ -30,8 +31,10 @@ abstract class BasicDto<T extends object> {
       );
       acc[camelKey] = this[propName];
       return acc;
-    }, {}) as SnakelizeKeys<T>;
+    }, {}) as SnakelizeKeys<this>;
   }
+
+  abstract clone<T>(data: object): T;
 }
 
 export { BasicDto };
